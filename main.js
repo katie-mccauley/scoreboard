18bfe25ab3e54ad856fcf174c4e20d5cdd6f9b8f
Katie McCauley <katiemccau02@gmail.com>
2022-02-07 20:14:21 -0700

    commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let teamScore1 = 0
 let teamScore2 = 0
 let touchScore1 = 0
 let touchScore2 = 0
+let safetyScore1 = 0
+let safetyScore2 = 0
 
 function increaseScore(team) {
   if (team == 'team1') {
@@ -15,19 +17,28 @@ function increaseScore(team) {
 function touchDown(team) {
   if (team == 'team1') {
     touchScore1 += 6
-    console.log(touchScore1);
   } else if (team == "team2") {
     touchScore2 += 6
-    console.log(touchScore2);
+  }
+  draw()
+}
+
+function increaseSafety(team) {
+  if (team == 'team1') {
+    safetyScore1 += 4
+  } else if (team == 'team2') {
+    safetyScore2 += 4
   }
   draw()
 }
 
 function draw() {
   document.getElementById('team1').innerText = teamScore1
-  document.getElementById(`team2`).innerText = teamScore2
+  document.getElementById('team2').innerText = teamScore2
   document.getElementById('touch1').innerText = touchScore1
   document.getElementById('touch2').innerText = touchScore2
+  document.getElementById('safety1').innerText = safetyScore1
+  document.getElementById('saftey2').innerText = safetyScore2
 }
 
 
@@ -37,6 +48,8 @@ function reset() {
   teamScore2 = 0
   touchScore1 = 0
   touchScore2 = 0
+  safetyScore1 = 0
+  safetyScore2 = 0
   draw()
 }
 
